03b34f54628b2bb6ca0ab878e4102b83fc0eabce
prashantpkc <prashantkumarpkc@gmail.com>
2023-10-06 12:43:06 +0530

    give authorize to admin for ftch frnahcise bank details

--- routes/frenchise/fetch-bank-details.js
+++ routes/frenchise/fetch-bank-details.js
@@ -4,5 +4,5 @@ const router = express.Router();
 const { isAuthenticated, authorizeRole } = require('../../middleware/checkAuth');
 const { fetchFranchiseBankDetails } = require('../../controllers/franchiseController');
 
-router.post('/frenchise/fetch-bank-details',isAuthenticated,authorizeRole(["franchise"]),fetchFranchiseBankDetails);
+router.post('/frenchise/fetch-bank-details',isAuthenticated,authorizeRole(["franchise", "admin"]),fetchFranchiseBankDetails);
 module.exports = router;
