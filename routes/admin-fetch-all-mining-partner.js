e710bf33849cf80efb299e0d79963501a5238df8
prashantpkc <prashantkumarpkc@gmail.com>
2024-02-26 16:42:06 +0530

    post to get

--- routes/admin-fetch-all-mining-partner.js
+++ routes/admin-fetch-all-mining-partner.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { isAuthenticated, authorizeRole } = require('../middleware/checkAuth');
 const { adminFetchAllMiningPartner } = require('../controllers/adminControllers');
 
-router.post('/admin-fetch-all-mining-partner',isAuthenticated, authorizeRole([ "admin"]), adminFetchAllMiningPartner);
+router.get('/admin-fetch-all-mining-partner',isAuthenticated, authorizeRole([ "admin"]), adminFetchAllMiningPartner);
 
 
 
